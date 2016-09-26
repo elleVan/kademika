169eb38d882f436a5ed3f8ce3163315da647c295
Mary <zmi13@list.ru>
2016-09-26 06:10:18 +0300

    f_3_1_9 - create my exception

---
+++ src/lessonsJD/lesson_3/F_3_1_4.java
@@ -0,0 +1,20 @@
+package lessonsJD.lesson_3;
+
+public class F_3_1_4 {
+
+    public static void main(String[] args) {
+        try {
+            doSmth();
+        } catch (InterruptedException e) {
+            System.out.println("Do smth with e");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void doSmth() throws InterruptedException {
+
+        Thread.currentThread().interrupt();
+        Thread.sleep(1000);
+    }
+}
