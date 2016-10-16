8c017d4b5ec3a05e063ba15b08439cfbf2c33464
Mary <zmi13@list.ru>
2016-10-16 16:01:28 +0300

    f_4_2_3 - move Node to SimpleLinkedList

--- src/lessonsJD/lesson4/SimpleLinkedList.java
+++ src/lessonsJD/lesson4/SimpleLinkedList.java
@@ -8,54 +8,18 @@ public class SimpleLinkedList {
     public SimpleLinkedList() {
     }
 
+    public class Node {
+        private Object o;
+        private Node node;
+    }
+
     public void addFirst(Object o) {
-        root = new Node(o, root);
-        size++;
     }
 
     public void addLast(Object o) {
-        if (root == null) {
-            addFirst(o);
-            return;
-        } else {
-            Node node = root;
-            while (node.getNode() != null) {
-                node = node.getNode();
-            }
-            Node newNode = new Node(o, null);
-            node.setNode(newNode);
-        }
-        size++;
     }
 
     public void addAfter(Object o, Object prev) {
-        if (root != null) {
-            Node node = root;
-            while (node.getNode() != null) {
-                if (node.getO() == prev) {
-                    break;
-                }
-                node = node.getNode();
-            }
-            if (node.getO() != prev) {
-                return;
-            }
-            Node next = node.getNode();
-            Node newNode = new Node(o, next);
-            node.setNode(newNode);
-        }
-        size++;
-    }
-
-    public void printList() {
-        if (root != null) {
-            System.out.println(root.getO().toString());
-            Node node = root;
-            while (node.getNode() != null) {
-                node = node.getNode();
-                System.out.println(node.getO().toString());
-            }
-        }
     }
 
     public int getSize() {
