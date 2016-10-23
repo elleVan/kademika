39b0376e2eb9f9a33c443889382d54860745befa
Mary <zmi13@list.ru>
2016-10-23 11:11:57 +0300

    f_4_3_8 - add Address field

--- src/lessonsJD/lesson4/Person.java
+++ src/lessonsJD/lesson4/Person.java
@@ -5,16 +5,25 @@ public class Person {
     private String name;
     private int age;
     private long salary;
+    private Address address;
 
     public Person(String name, int age, long salary) {
         this.name = name;
         this.age = age;
         this.salary = salary;
+        address = new Address("City", "Street", 10);
     }
 
     @Override
     public boolean equals(Object obj) {
-        return this.hashCode() == obj.hashCode();
+        if (obj instanceof Person) {
+            Person p = (Person) obj;
+            if (name != null && name.equals(p.name) && age == p.age && salary == p.salary &&
+                    address != null && address.equals(p.address)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -22,9 +31,45 @@ public class Person {
         int result = 34;
 
         result = result * 37 + name.hashCode();
-        result = result * 37 + new Integer(age).hashCode();
+        result = result * 37 + age;
         result = result * 37 + new Long(salary).hashCode();
+        result = result * 37 + address.hashCode();
 
         return result;
+    }
+
+    private class Address {
+        String city;
+        String street;
+        int house;
+
+        public Address(String city, String street, int house) {
+            this.city = city;
+            this.street = street;
+            this.house = house;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Address) {
+                Address a = (Address) obj;
+                if (city != null && city.equals(a.city) && street != null && street.equals(a.street)
+                        && house == a.house) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 23;
+
+            result = result * 37 + city.hashCode();
+            result = result * 37 + street.hashCode();
+            result = result * 37 + house;
+
+            return result;
+        }
     }
 }
