9749b04f3b2acc228b9a1e36389da41ec463c249
Mary <zmi13@list.ru>
2016-10-30 08:58:04 +0200

    f_5_1_4 - create window

---
+++ src/lessonsJD/lesson5/F_5_1_4.java
@@ -0,0 +1,15 @@
+package lessonsJD.lesson5;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class F_5_1_4 {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setVisible(true);
+        f.setLocation(300, 100);
+        f.setMinimumSize(new Dimension(800, 600));
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
