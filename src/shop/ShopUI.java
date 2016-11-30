67b54800c1c1b266f1b146848e509cf608eb1b6d
Mary <zmi13@list.ru>
2016-11-30 10:19:23 +0200

    f_5_4_4 - shop - carry out transactions by clicking

--- src/shop/ShopUI.java
+++ src/shop/ShopUI.java
@@ -2,11 +2,14 @@ package shop;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
 public class ShopUI {
 
     private Shop shop;
+    private String sweetName;
 
     public ShopUI(Shop shop) {
         this.shop = shop;
@@ -28,14 +31,20 @@ public class ShopUI {
 
         JLabel lName = new JLabel("Your name:");
         JTextField tfName = new JTextField(10);
-        panel.add(lName, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.PAGE_START, 0, new Insets(0, 0, 0, 5), 0, 0));
-        panel.add(tfName, new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.PAGE_START, 0, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(lName, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(0, 0, 0, 5), 0, 0));
+        panel.add(tfName, new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(0, 0, 0, 0), 0, 0));
 
         JLabel lButtons = new JLabel("Sweets:");
         JPanel buttons = new JPanel(new GridLayout(0, 1));
         ButtonGroup group = new ButtonGroup();
         for (String[] el : shop.getSweets()) {
             JRadioButton button = new JRadioButton(el[Shop.NAME]);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    sweetName = e.getActionCommand();
+                }
+            });
             buttons.add(button);
             group.add(button);
         }
@@ -54,7 +63,16 @@ public class ShopUI {
         JButton button = new JButton("Buy");
         panel.add(button, new GridBagConstraints(1, 3, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(10, 0, 0, 0), 0, 0));
 
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                shop.newTransaction(shop.newCustomer(tfName.getText()), new Sweet[] {
+                        shop.newSweet(sweetName, Integer.parseInt(tfQuantity.getText())),
+                });
+                shop.printBase();
+            }
+        });
+
         return panel;
     }
-
 }
