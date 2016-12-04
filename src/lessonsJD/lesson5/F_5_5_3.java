072d277c8c9d012e84c833d206e89bacc5457fd3
Mary <zmi13@list.ru>
2016-12-04 11:03:10 +0200

    f_5_5_3 - magic square

---
+++ src/lessonsJD/lesson5/F_5_5_3.java
@@ -0,0 +1,8 @@
+package lessonsJD.lesson5;
+
+public class F_5_5_3 {
+
+    public static void main(String[] args) {
+        Square square = new Square();
+    }
+}
---
+++ src/lessonsJD/lesson5/Square.java
@@ -0,0 +1,70 @@
+package lessonsJD.lesson5;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+
+public class Square {
+
+    private Color color = Color.BLUE;
+
+    public Square() {
+        JFrame frame = new JFrame();
+        frame.setLocation(500, 150);
+        frame.setMinimumSize(new Dimension(400, 350));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(createPanel());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JPanel createPanel() {
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.setColor(color);
+                g.fillRect(100, 100, 100, 100);
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(100, 100));
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Random random = new Random();
+                int r = random.nextInt(256);
+                int g = random.nextInt(256);
+                int b = random.nextInt(256);
+                color = new Color(r, g, b);
+                panel.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        return panel;
+    }
+
+}
