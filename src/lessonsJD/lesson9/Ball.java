cda05c99eb1b19be9406c6f588d39c13efc81a8a
Mary <zmi13@list.ru>
2017-01-29 17:04:03 +0200

    f_9_1_3 - flying balls in threads

---
+++ src/lessonsJD/lesson9/Ball.java
@@ -0,0 +1,49 @@
+package lessonsJD.lesson9;
+
+import java.awt.*;
+
+public class Ball implements Runnable {
+
+    private int x;
+    private int y;
+
+    private Color color;
+    private int speed;
+
+    private boolean toLeft = false;
+
+    public Ball(int x, int y, Color color, int speed) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.speed = speed;
+    }
+
+    public void run() {
+
+        while (true) {
+            if (x > Balls.SIZE - 46) {
+                toLeft = true;
+            } else if (x < 0) {
+                toLeft = false;
+            }
+
+            if (toLeft) {
+                x -= speed * 2;
+            } else {
+                x += speed * 2;
+            }
+
+            try {
+                Thread.sleep(80);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, 30, 30);
+    }
+}
---
+++ src/lessonsJD/lesson9/Balls.java
@@ -0,0 +1,75 @@
+package lessonsJD.lesson9;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balls extends JPanel {
+
+    public static void main(String[] args) {
+
+        new Balls();
+    }
+
+    public static final int SIZE = 550;
+
+    private List<Ball> balls;
+
+    private Color[] colors = new Color[] {
+            Color.RED,
+            Color.ORANGE,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.CYAN,
+            Color.BLUE,
+            Color.MAGENTA
+    };
+
+    public Balls() {
+        balls = new ArrayList<>();
+        createWindow();
+        createBalls();
+
+        while (true) {
+            repaint();
+
+            try {
+                Thread.sleep(17);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void createBalls() {
+
+        int y = 0;
+        for (int i = 0; i < 7; i++) {
+            Ball ball = new Ball(0, y, colors[i], i + 1);
+            balls.add(ball);
+            new Thread(ball).start();
+            y += 70;
+        }
+    }
+
+    private void createWindow() {
+        JFrame frame = new JFrame("BALLS");
+        frame.setLocation(750, 150);
+        frame.setMinimumSize(new Dimension(SIZE, SIZE));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(this);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (Ball ball : balls) {
+            ball.draw(g);
+        }
+    }
+}
